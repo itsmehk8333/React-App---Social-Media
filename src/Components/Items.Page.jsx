8e16caa109330ee2ed42dd2cacb2f1c9b90650e0
Pranay Darla <gunnalaharikrishna@gmail.com>
2025-11-29 23:18:23 +0530

    added the loading animation

--- src/Components/Items.Page.jsx
+++ src/Components/Items.Page.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import getItems from "../apis/items.apis";
 import "./Items.css";
@@ -7,10 +7,18 @@ import { Link } from "react-router-dom";
 function ItemsPage() {
   const items = useSelector((store) => store.items);
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     dispatch(getItems());
   }, [dispatch]);
+
+  // when items arrive, stop loading
+  useEffect(() => {
+    if (items && items.length > 0) {
+      setLoading(false);
+    }
+  }, [items]);
 
   return (
     <div className="items-page">
@@ -18,7 +26,6 @@ function ItemsPage() {
         <h2 className="items-title">Social Media For Travellers</h2>
 
         <div className="search-wrapper">
-          {/* optional icon – you can swap for an SVG */}
           <span className="search-icon">🔍</span>
           <input
             type="text"
@@ -28,9 +35,11 @@ function ItemsPage() {
         </div>
 
         <div className="grid-container">
-          {items.map((item) => (
-            <ItemComponent key={item.id} item={item} />
-          ))}
+          {loading
+            ? Array.from({ length: 6 }).map((_, index) => (
+                <ItemSkeleton key={index} />
+              ))
+            : items.map((item) => <ItemComponent key={item.id} item={item} />)}
         </div>
       </div>
     </div>
@@ -39,25 +48,44 @@ function ItemsPage() {
 
 const ItemComponent = ({ item }) => {
   return (
-    <Link to={`/item/${item.id}`}  className="no-link-style">
+    <Link to={`/item/${item.id}`} className="no-link-style">
+      <div className="grid-box">
+        <div className="item-image">
+          <img
+            src={`https://picsum.photos/600/350?random=${item.id}`}
+            alt={item.title}
+          />
+        </div>
+
+        <div className="item-content">
+          <h3>{item.title}</h3>
+          <p>{item.body}</p>
+
+          <button className="item-cta">
+            <span>&gt;</span>
+          </button>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+// 💀 Skeleton card for loading state
+const ItemSkeleton = () => {
+  return (
     <div className="grid-box">
       <div className="item-image">
-        <img
-          src={`https://picsum.photos/600/350?random=${item.id}`}
-          alt={item.title}
-        />
+        <div className="skeleton skeleton-img" />
       </div>
 
       <div className="item-content">
-        <h3>{item.title}</h3>
-        <p>{item.body}</p>
+        <div className="skeleton skeleton-title" />
+        <div className="skeleton skeleton-text" />
+        <div className="skeleton skeleton-text short" />
 
-        <button className="item-cta">
-          <span>&gt;</span>
-        </button>
+        <div className="skeleton skeleton-btn" />
       </div>
     </div>
-    </Link>
   );
 };
 
